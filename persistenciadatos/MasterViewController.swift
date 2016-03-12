a4597254af947d57c70e198f3f9f0327c0abb7d5
Raul Suarez Dabo <raul@suarez.com.es>
2016-03-12 10:46:15 +0100

    Change order

--- persistenciadatos/MasterViewController.swift
+++ persistenciadatos/MasterViewController.swift
@@ -121,7 +121,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
